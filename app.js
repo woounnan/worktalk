a56b38c3851744741a9e17075f536764d25ff2fa
unknown <woounnan@gmail.com>
2019-09-11 19:44:44 +0900

    add login on be

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./api');
+var apiRouter = require('./apis');
 
 var app = express();
 
