eca33213fae3b780664b483a9790ad65a006169b
unknown <woounnan@gmail.com>
2019-09-11 19:51:16 +0900

    add login on be

--- apis/index.js
+++ apis/index.js
@@ -1,11 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var express = require('express')
+var router = express.Router()
+var createError = require('http-errors')
 
 router.use('/login', require('./login'))
 
-module.exports = router;
+
+module.exports = router
