42e8b1f3a81ac7941c14cfbbf0105ab5094a16ab
unknown <woounnan@gmail.com>
2019-09-11 22:17:59 +0900

    add declaration of cors in app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors =require('cors')
+
+app.use(cors())
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
