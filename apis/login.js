7b55a22b7c97eb8c2befe0a9721c468c116e6479
unknown <woounnan@gmail.com>
2019-09-11 19:50:24 +0900

    add login on be

--- apis/login.js
+++ apis/login.js
@@ -2,6 +2,18 @@ var express = require('express')
 var createError = require('http-errors')
 var router = express.Router()
 
+
+router.get('/', (req, res) => {
+	const {id , pwd} = req.body
+	if(!id || !pwd){
+		console.log('failed login')
+		return res.send({success: false, msg: 'invalid input values.'})
+	}
+
+	res.send({ success: true, toke: 'Login succeeded.'})
+	console.log('succeeded login')
+})
+
 router.post('/', (req, res) => {
 	const {id , pwd} = req.body
 	if(!id || !pwd){
