d68233288b0a4cf6788c4aed09958f688a11c28b
unknown <woounnan@gmail.com>
2019-09-12 15:32:18 +0900

    a

--- apis/test_mg.js
+++ apis/test_mg.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
 	password: { type: String, required: true, unique: true, trim: true }
 })
 
-var Users = mongoose.model('Users', userSchema)
+var Users = mongoose.model('Users', userSchema, 'Test')
 
 var users = new Users('Bobasdf', 'bobob')
 users.save( ( e, user) =>{
