1c7caa8ef0dc8e23f7e62dab131a95360ccdc908
unknown <woounnan@gmail.com>
2019-09-12 18:55:49 +0900

    a

--- apis/test_mg.js
+++ apis/test_mg.js
@@ -1,6 +1,9 @@
+
 var mongoose = require('mongoose')
 var User = require('./model')
-
+var express = require('express')
+var createError = require('http-errors')
+var Router = express.Router()
 console.log('running test pages of mongoose')
 /*
 const userSchema = new mongoose.Schema({
@@ -15,3 +18,5 @@ users.save( ( e, user) =>{
 	if(e) return console.error('login.js::10....occurred at users.save')
 		console.log('User created')
 })
+
+module.exports = Router
