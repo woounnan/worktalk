476c59ff77f110622430aa4b4c1292293c18d922
unknown <woounnan@gmail.com>
2019-09-12 19:00:18 +0900

    a

--- apis/index.js
+++ apis/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 var createError = require('http-errors')
 
-console.log('here is api.js')
+console.log('here is at api.js')
 router.use('/login', require('./login'))
 
 router.use('/test', require('./test_mg'))
