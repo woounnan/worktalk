5c62ab63293443ccaa6fad13cae93bd40159aeda
unknown <woounnan@gmail.com>
2019-09-12 23:32:47 +0900

    a

--- apis/login.js
+++ apis/login.js
@@ -11,10 +11,14 @@ router.post('/', (req, res) => {
 	const {id , pw} = req.body
 	User.findOne({id: id, password: pw}, (e, user) =>{
 		if(e) {
+			console.log('error occurred')
+			console.error(e)
+			return res.send({code: 0})
+		}
+		if(!user){
 			console.log('login failed')
 			return res.send({code: 0})
 		}
-		console.log('find the user : ' + user.id)
 		console.log('login succeeded')
 		res.send({code: 1})
 	})
