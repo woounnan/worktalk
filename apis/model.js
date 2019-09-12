5c83080c64aadef4431977156d8c95929ea6cd10
unknown <woounnan@gmail.com>
2019-09-12 23:54:39 +0900

    a

--- apis/model.js
+++ apis/model.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
 	id: { type: String, required: true, unique: true, trim: true, index: true},
 	password: { type: String, required: true, unique: true, trim: true }
+	key: { type: String, unique: true, default: Date.now}
 })
 
 
 
-var User = mongoose.model('User', userSchema, 'userCol')
+var User = mongoose.model('User', userSchema, 'myTable')
 
 module.exports = User
