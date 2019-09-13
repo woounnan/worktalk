6fa777b148c024dcccbf062daa44e75e788265f7
unknown <woounnan@gmail.com>
2019-09-13 17:03:52 +0900

    a

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -14,7 +14,7 @@
             sm="8"
             md="4"
           >
-            <modals-container />
+
             <v-card class="elevation-12">
               <v-toolbar
                 color="primary"
