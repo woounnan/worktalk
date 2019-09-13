19b305eddd6b0a95f909e3df7b0fd5768c9d8be8
unknown <woounnan@gmail.com>
2019-09-13 19:06:25 +0900

    a

--- front/src/main.js
+++ front/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-
+window.Vue = Vue;
 Vue.config.productionTip = false
 
 new Vue({
