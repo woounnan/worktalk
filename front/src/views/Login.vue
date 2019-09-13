07e2d61fdb1f9ec2d1c82f0be44a8185ec816c25
unknown <woounnan@gmail.com>
2019-09-13 17:12:03 +0900

    a

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -14,7 +14,7 @@
             sm="8"
             md="4"
           >
-            <modals-container />
+            <modals-container :test=this.test/>
             <v-card class="elevation-12">
               <v-toolbar
                 color="primary"
@@ -92,7 +92,6 @@
         //location.href = '/reg' //normal move
         this.$modal.show(Register,
           {
-            test : false,
             modal : this.$modal
           },
           {
