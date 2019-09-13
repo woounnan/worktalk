1f89e9f0398df73b92e2b59be30aec4137bd3620
unknown <woounnan@gmail.com>
2019-09-13 12:41:39 +0900

    a

--- front/src/views/Register.vue
+++ front/src/views/Register.vue
@@ -1,4 +1,90 @@
 <template>
+  <v-row justify="center">
+    <v-col cols="12" sm="8">
+      <v-card>
+        <v-card-title class="cyan darken-1">
+          <span class="headline white--text">Sarah Mcbeal</span>
+
+          <div class="flex-grow-1"></div>
+          <v-btn dark icon>
+            <v-icon>check</v-icon>
+          </v-btn>
+
+          <v-btn dark icon>
+            <v-icon>cancel</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-list>
+          <v-list-item @click="">
+            <v-list-item-action>
+              <v-icon>mdi-phone</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>(650) 555-1234</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+
+              <v-icon>mdi-message-text</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item @click="">
+            <v-list-item-action>
+              <v-icon>mdi-phone</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>(323) 555-6789</v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-icon>mdi-message-text</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item @click="">
+            <v-list-item-action>
+              <v-icon>mdi-email</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>mcbeal@example.com</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item @click="">
+            <v-list-item-action>
+              <v-icon>mdi-map-marker</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>Orlando, FL 79938</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-img
+          src="https://picsum.photos/700?image=996"
+          height="200px"
+        ></v-img>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+
+<template>
+
+
+
     <div class="container">
         <div class="input-group col-md-3">
             <span class = "input-group-addon">
