173c08a21b216e495cf59f0b816df57516780ee8
unknown <woounnan@gmail.com>
2019-09-13 15:23:11 +0900

    a

--- front/src/views/Register.vue
+++ front/src/views/Register.vue
@@ -96,7 +96,7 @@ export default {
       closeWindow(){
         console.log('call the closeWindow function');
         this.$emit('close')
-        console.log(this.$root)
+        console.log(this.$root.$on)
       }
   }
 }
